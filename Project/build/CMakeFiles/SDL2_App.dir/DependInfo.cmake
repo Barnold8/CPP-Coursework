17fa57f6c15c89838f7a3b396d66194fc873ee0a
Brandon Wright <psybw7@nottingham.ac.uk>
2023-03-03 15:00:17 +0000

    Add: Cmake++ added

---
+++ Project/build/CMakeFiles/SDL2_App.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/Code/C++/CW/Project/src/BaseEngine.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/BaseEngine.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/BouncingBall.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/BouncingBall.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/BouncingBallMain.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/BouncingBallMain.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/DisplayableObject.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/DisplayableObject.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/DraggingDemo.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/DraggingDemo.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/DrawingSurface.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/DrawingSurface.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/FlashingDemo.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/FlashingDemo.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/FontManager.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/FontManager.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/ImageMappingDemo.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/ImageMappingDemo.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/MazeDemoMain.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/MazeDemoMain.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/MazeDemoObject.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/MazeDemoObject.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/MazeDemoTileManager.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/MazeDemoTileManager.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/SimpleDemo.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/SimpleDemo.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/SimpleImage.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/SimpleImage.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/StarfieldDemo.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/StarfieldDemo.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/TileManager.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/TileManager.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/ZoomingDemo.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/ZoomingDemo.cpp.o"
+  "/mnt/d/Code/C++/CW/Project/src/mainfunction.cpp" "/mnt/d/Code/C++/CW/Project/build/CMakeFiles/SDL2_App.dir/src/mainfunction.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
