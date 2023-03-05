8ce228cc87946cef9e6fe282499155a278403e5c
Brandon Wright <psybw7@nottingham.ac.uk>
2023-03-05 14:12:43 +0000

    Add: added demo folder and removed sh files since they corrupt

---
+++ Demo/build/CMakeFiles/SDL2_App.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/BaseEngine.cpp" "CMakeFiles/SDL2_App.dir/src/BaseEngine.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/BaseEngine.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/BouncingBall.cpp" "CMakeFiles/SDL2_App.dir/src/BouncingBall.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/BouncingBall.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/BouncingBallMain.cpp" "CMakeFiles/SDL2_App.dir/src/BouncingBallMain.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/BouncingBallMain.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/DisplayableObject.cpp" "CMakeFiles/SDL2_App.dir/src/DisplayableObject.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/DisplayableObject.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/DraggingDemo.cpp" "CMakeFiles/SDL2_App.dir/src/DraggingDemo.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/DraggingDemo.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/DrawingSurface.cpp" "CMakeFiles/SDL2_App.dir/src/DrawingSurface.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/DrawingSurface.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/FlashingDemo.cpp" "CMakeFiles/SDL2_App.dir/src/FlashingDemo.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/FlashingDemo.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/FontManager.cpp" "CMakeFiles/SDL2_App.dir/src/FontManager.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/FontManager.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/ImageMappingDemo.cpp" "CMakeFiles/SDL2_App.dir/src/ImageMappingDemo.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/ImageMappingDemo.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/MazeDemoMain.cpp" "CMakeFiles/SDL2_App.dir/src/MazeDemoMain.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/MazeDemoMain.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/MazeDemoObject.cpp" "CMakeFiles/SDL2_App.dir/src/MazeDemoObject.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/MazeDemoObject.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/MazeDemoTileManager.cpp" "CMakeFiles/SDL2_App.dir/src/MazeDemoTileManager.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/MazeDemoTileManager.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/SimpleDemo.cpp" "CMakeFiles/SDL2_App.dir/src/SimpleDemo.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/SimpleDemo.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/SimpleImage.cpp" "CMakeFiles/SDL2_App.dir/src/SimpleImage.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/SimpleImage.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/StarfieldDemo.cpp" "CMakeFiles/SDL2_App.dir/src/StarfieldDemo.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/StarfieldDemo.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/TileManager.cpp" "CMakeFiles/SDL2_App.dir/src/TileManager.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/TileManager.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/ZoomingDemo.cpp" "CMakeFiles/SDL2_App.dir/src/ZoomingDemo.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/ZoomingDemo.cpp.o.d"
+  "/home/barnold/Code/C++/CW/CPP-Coursework/Demo/src/mainfunction.cpp" "CMakeFiles/SDL2_App.dir/src/mainfunction.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/mainfunction.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- Project-CW2/build_and_run.sh
+++
@@ -1,6 +0,0 @@
-# This file was made to make the build process easier, avoiding any ambuiguity of building the program in any other dir than source
-
-
-cd build
-
-cmake ../ && make && ./SDL2_App
