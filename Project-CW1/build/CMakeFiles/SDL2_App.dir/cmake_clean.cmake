c80a384c85893bdae928e7ace595568393be0479
Brandon Wright <psybw7@nottingham.ac.uk>
2023-03-09 17:06:11 +0000

    Add: file structure works without crappy demo code

---
+++ Project-CW1/build/CMakeFiles/SDL2_App.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDL2_App.dir/ProjectSrc/cw1_main.cpp.o"
+  "CMakeFiles/SDL2_App.dir/src/BaseEngine.cpp.o"
+  "CMakeFiles/SDL2_App.dir/src/DisplayableObject.cpp.o"
+  "CMakeFiles/SDL2_App.dir/src/DrawingSurface.cpp.o"
+  "CMakeFiles/SDL2_App.dir/src/FontManager.cpp.o"
+  "CMakeFiles/SDL2_App.dir/src/SimpleImage.cpp.o"
+  "CMakeFiles/SDL2_App.dir/src/TileManager.cpp.o"
+  "CMakeFiles/SDL2_App.dir/src/mainfunction.cpp.o"
+  "SDL2_App"
+  "SDL2_App.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDL2_App.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
