a8faefe81de70743da98fbf0e38f002edc718f61
Brandon Wright <psybw7@nottingham.ac.uk>
2023-03-09 18:16:33 +0000

    Add: Sky done

--- Project-CW1/ProjectSrc/cw1_main.cpp
+++ Project-CW1/ProjectSrc/cw1_main.cpp
@@ -5,15 +5,15 @@
 void Main::virtSetupBackgroundBuffer(){
 
     int colIndex = 0;
-    int colour = 0;
-    int iters = (getWindowHeight()/8)/0xF;
+    int colour = 0x478aed;
+    int iters = (getWindowHeight()/8)/0x5;
     
     for(int iY = 0; iY < getWindowHeight(); iY++){      
         for(int iX = 0; iX < getWindowWidth(); iX++){   
             setBackgroundPixel(iX,iY,colour);
         }    
-        if(iY % iters == 0){colour += 0x010101;}
-         
+        if(iY % iters == 0){colour += 0x010100;}
+        
     }
 
 
