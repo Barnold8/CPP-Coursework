0eb52f7fc3dac922dc9e3f3e4e5c727178f6b991
Brandon Wright <psybw7@nottingham.ac.uk>
2023-03-16 08:45:08 +0000

    Add: text is no longer obliterated since its out of tile range

--- Project-CW1/build/CMakeFiles/SDL2_App.dir/DependInfo.cmake
+++ Project-CW1/build/CMakeFiles/SDL2_App.dir/DependInfo.cmake
@@ -1,27 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/ProjectSrc/Bird.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/ProjectSrc/Bird.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/ProjectSrc/Pipe.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/ProjectSrc/Pipe.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/ProjectSrc/PipePair.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/ProjectSrc/PipePair.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/ProjectSrc/TileMap.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/ProjectSrc/TileMap.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/ProjectSrc/cw1_main.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/ProjectSrc/cw1_main.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/src/BaseEngine.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/src/BaseEngine.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/src/DisplayableObject.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/src/DisplayableObject.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/src/DrawingSurface.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/src/DrawingSurface.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/src/FontManager.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/src/FontManager.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/src/SimpleImage.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/src/SimpleImage.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/src/TileManager.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/src/TileManager.cpp.o"
-  "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/src/mainfunction.cpp" "/mnt/d/Code/C++/CPP-Coursework/Project-CW1/build/CMakeFiles/SDL2_App.dir/src/mainfunction.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/include/SDL2"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/ProjectSrc/Bird.cpp" "CMakeFiles/SDL2_App.dir/ProjectSrc/Bird.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/ProjectSrc/Bird.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/ProjectSrc/Pipe.cpp" "CMakeFiles/SDL2_App.dir/ProjectSrc/Pipe.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/ProjectSrc/Pipe.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/ProjectSrc/PipePair.cpp" "CMakeFiles/SDL2_App.dir/ProjectSrc/PipePair.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/ProjectSrc/PipePair.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/ProjectSrc/TileMap.cpp" "CMakeFiles/SDL2_App.dir/ProjectSrc/TileMap.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/ProjectSrc/TileMap.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/ProjectSrc/cw1_main.cpp" "CMakeFiles/SDL2_App.dir/ProjectSrc/cw1_main.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/ProjectSrc/cw1_main.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/src/BaseEngine.cpp" "CMakeFiles/SDL2_App.dir/src/BaseEngine.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/BaseEngine.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/src/DisplayableObject.cpp" "CMakeFiles/SDL2_App.dir/src/DisplayableObject.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/DisplayableObject.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/src/DrawingSurface.cpp" "CMakeFiles/SDL2_App.dir/src/DrawingSurface.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/DrawingSurface.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/src/FontManager.cpp" "CMakeFiles/SDL2_App.dir/src/FontManager.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/FontManager.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/src/SimpleImage.cpp" "CMakeFiles/SDL2_App.dir/src/SimpleImage.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/SimpleImage.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/src/TileManager.cpp" "CMakeFiles/SDL2_App.dir/src/TileManager.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/TileManager.cpp.o.d"
+  "/home/barnold/Code/C++/CPP-Coursework/Project-CW1/src/mainfunction.cpp" "CMakeFiles/SDL2_App.dir/src/mainfunction.cpp.o" "gcc" "CMakeFiles/SDL2_App.dir/src/mainfunction.cpp.o.d"
   )
 
 # Targets to which this target links.
